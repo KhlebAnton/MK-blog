a470326ddf184326ac670f6092e0ece3e3bf2d7a
Anton Khleb <khleb.4nton@yandex.ru>
2026-02-20 19:12:31 +0500

    fix

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мастерская красоты, парикмахерская, окрашивание волос | Владивосток, Русская, д. 52</title>
+    <link rel="stylesheet" href="./styles/style.css">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+	
+</head>
+
+<body>
+    <header class="header">
+        <div class="container header-container">
+            <a href="/" class="logo-wrapper">
+                <img src="./images/logo.jpg" alt="Логотип">
+            </a>
+            <nav class="header_nav">
+                <a href="#" class="header_nav__item is-active">
+                    Главная
+                </a>
+                <a href="#" class="header_nav__item">
+                    О нас
+                </a>
+                <a href="#" class="header_nav__item">
+                    Обучение
+                </a>
+                <a href="tel:+79874502136" class="header_nav__item header_phone">+7 908 961-9148</a>
+            </nav>
+        </div>
+    </header>
+    <main class="main">
+        <section class="product_section">
+            <div class="container">
+                <div class="product__content">
+                    <div class="product__content_top">
+                        <div class="product__content_top__img-wrapper">
+                            <img src="./images/3.jpg" alt=" фото 3" class="product__content_top__img">
+                        </div>
+                        <div class="product__content__info">
+                            <div class="product__content__info_top">
+                                <h1 class="product__content__title">Мы создаём шедевры!</h1>
+                                <p class="product__content__subtitle">
+                                    ...и произведения искусства из локонов
+                                </p>
+                            </div>
+                            <div class="product__content__info_middle">
+                                <p>
+                                 
+								
+								
+								В «Мастерской красоты» работают не просто мастера, а настоящие художники, чьи руки и ножницы творят чудеса. 
+								Каждый специалист — это проверенный годами профессионал, владеющий секретами мастерства и постоянно совершенствующий свои навыки. 
+								От элегантных стрижек и окрашиваний до изысканного маникюра и педикюра, от омолаживающих процедур до полного релакса, — здесь предлагают весь спектр услуг, призванных подчеркнуть 
+								уникальность каждого клиента. Внимание к деталям, индивидуальный подход и искреннее желание подарить радость – вот что отличает мастеров «Мастерской красоты».
+								
+							    </p>
+                               
+							   
+                            </div>
+                            <a href="#" class="btn">Записаться</a>
+                        </div>
+                    </div>
+                    <div class="product__content_bottom">
+                        <div class="product__content__gallery">
+                            <img src="./images/1.jpg" alt="фото 1">
+                            <img src="./images/2.jpg" alt="фото 2">
+                            <img src="./images/4.jpg" alt="фото 4">
+                            <img src="./images/5.jpg" alt="фото 5">
+                            
+                        </div>
+<a href="tel:+79874502136" class="product__content__gallery_footer">
+                                    <div class="gallery_footer__title">Окрашивание волос</div>
+                                    <span class="gallery_footer__subtitle">
+									
+Здесь существует множество вариаций: от полного покрытия волос одним тоном до сложных техник, таких как мелирование, балаяж или омбре, требующих особой точности и художественного видения. 
+									
+									</span>
+
+                               
+                               
+
+                            </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
